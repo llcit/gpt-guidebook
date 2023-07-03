1c1ab53747c12f68db9215acec16fc17b59413c7
asnicho5 <91578416+asnicho5@users.noreply.github.com>
2023-07-03 14:05:59 -0700

    Set up Models and Admin
    I set up all the models and made sure they have the desirable parameters. I also configured the admin section of the website to be better to use.

---
+++ guidebook/prompt/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+
+from .models import Prompt, Category, Section, Paragraph
+
+# Register your models here.
+
+class PromptInline(admin.TabularInline):
+    model = Prompt
+    extra = 1
+
+class ParagraphInline(admin.TabularInline):
+    model = Paragraph
+    extra = 1
+
+class SectionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["section_title"]}),
+        (None, {"fields": ["section_description"]}),
+        (None, {"fields": ["section_rank"]}),
+    ] 
+    inlines = [ParagraphInline]
+    list_display = ["section_title", "section_rank"]
+
+    def sortByRank(self, obj):
+        return obj.section_rank
+    
+    sortByRank.admin_order_field =  "section_rank"
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["category_name"]}),
+        (None, {"fields": ["category_information"]})
+    ]
+    inlines = [PromptInline]
+
+
+
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ guidebook/prompt/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    category_information = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
+    
+class Prompt(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    prompt_title = models.CharField(max_length=255, null=True)
+    prompt_text = models.TextField(null=True)
+    prompt_output = models.TextField(null=True)
+    prompt_language = models.CharField(max_length=255, null=True)
+    prompt_warning = models.BooleanField(default=False)
+
+class Section(models.Model):
+    section_title = models.CharField(max_length=255)
+    section_rank = models.IntegerField(default=0)
+    section_description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.section_title
+
+class Paragraph(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    paragraph_title = models.CharField(max_length=255, null=True)
+    paragraph_text = models.TextField(null=True)
+    
